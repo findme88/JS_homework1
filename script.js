14820cb2521dc00f38838e7ca1503d0f83e0ece3
zordexxx <zordexxx69@gmail.com>
2020-04-29 09:58:30 +0300

    1/10

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+1.1
+
+let yourName = prompt("What is your name?");
+alert(`Hello, ${yourName}`);
+
+1.2
+
+const CURRENT_YEAR = 2020;
+    let age = prompt('What year were you born in?');
+    alert(`Your age is ${CURRENT_YEAR - age}`);
+
+1.3
+
+let sideSquare = prompt('Enter the side of the square')
+console.log (`The perimeter of the quarter is ${sideSquare * 4}`)
+
+1.4 
+
+let circleRadius =  prompt('Enter the circle radius')
+console.log (`The circle area is ${Math.PI * (circleRadius ** 2)}`)
+
+1.5
+
+let range = prompt ('Enter distance between cities'), time = prompt ('how many hours do you want to travel?')
+console.log(`You need to move with ${range / time} km/h speed`)
+
+1.6
+
+const USD_EUR = 0.92
+let usd = prompt ('how many dollars do you want to exchange?')
+console.log (`You'll get ${usd * USD_EUR} eur`)
+
+1.7 
+
+let flashSize = prompt ('Enter your flash drive size in gb')
+console.log (`At the flash drive will fit ${Math.trunc(flashSize*1024/820)} files`)
+
+1.8
+
+let sum = prompt ('Enter your wallet balance'), price = prompt ('Enter the price of one chocolate bar')
+console.log (`You can buy ${Math.trunc(sum / price)} chocolates and your change ${sum % price}`)
+
+1.9
+
+let num = prompt ('Enter a three digit number')
+function reverse(num, r = 0){
+    if(!num) return r;
+    r =  r * 10  + num % 10;
+    return reverse(Math.floor( num / 10), r);
+}
+console.log(reverse(num));
+
+1.10
+
+let sum = prompt ('Enter deposit amount')
+console.log (`in 2 months you will receive ${sum * 0.05 / 6}`)
+
+
+
+
+
