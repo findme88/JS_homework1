71cbb01691528c78f43750227ec50d06fa84a1ac
zordexxx <zordexxx69@gmail.com>
2020-05-05 16:06:03 +0300

    40/40

--- script3.js
+++ script3.js
@@ -1,94 +1,314 @@
-"use strict";
+// "use strict";
 
-3.1;
+// 3.1;
 
-let $title1 = document.createElement("h1");
-$title1.innerHTML =
-  "Подсчитать сумму всех чисел в заданном пользователем диапазоне.";
-document.body.append($title1);
+// let $title1 = document.createElement("h1");
+// $title1.innerHTML =
+//   "Подсчитать сумму всех чисел в заданном пользователем диапазоне.";
+// document.body.append($title1);
 
-let $task1 = document.createElement("div");
-$task1.classList.add("task1");
-$task1.innerHTML =
-  '<input class="input_first" placeholder="initial number" type="number"><input class="input_last" placeholder="finite number" type="number"></input><button>calculate</button>';
-document.body.append($task1);
-let $btn1 = $task1.querySelector("button");
-let $result1 = document.createElement("div");
-$result1.innerHTML = "<h1></h1>";
-$result1.style = "font-size: 40px";
-document.body.append($result1);
+// let $task1 = document.createElement("div");
+// $task1.classList.add("task1");
+// $task1.innerHTML =
+//   '<input class="input_first" placeholder="initial number" type="number"><input class="input_last" placeholder="finite number" type="number"></input><button>calculate</button>';
+// document.body.append($task1);
+// let $btn1 = $task1.querySelector("button");
+// let $result1 = document.createElement("div");
+// $result1 = document.createElement ('h1')
+// $result1.style = "font-size: 40px";
+// document.body.append($result1);
 
-let getResult1 = () => {
-  let num1 = $task1.querySelector(".input_first").value;
-  let num2 = $task1.querySelector(".input_last").value;
-  let sum = 0;
-  while (num1 <= num2) {
-    sum += +num1;
-    num1++;
-  }
-  $result1.innerHTML = `The sum of the numbers in the range is ${sum}`;
-};
+// let getResult1 = () => {
+//   let num1 = $task1.querySelector(".input_first").value;
+//   let num2 = $task1.querySelector(".input_last").value;
+//   let sum = 0;
+//   while (num1 <= num2) {
+//     sum += +num1;
+//     num1++;
+//   }
+//   $result1.innerHTML = `The sum of the numbers in the range is ${sum}`;
+// };
 
-$btn1.onclick = getResult1;
+// $btn1.onclick = getResult1;
 
-3.2
+// 3.2;
 
-let $title2 = document.createElement("h1");
-$title2.innerHTML =
-  "Запросить 2 числа и найти только наибольший общий делитель.";
-document.body.append($title2);
+// let $title2 = document.createElement("h1");
+// $title2.innerHTML =
+//   "Запросить 2 числа и найти только наибольший общий делитель.";
+// document.body.append($title2);
 
-let $task2 = document.createElement("div");
-$task2.classList.add("task2");
-$task2.innerHTML =
-  '<input class="input_1" placeholder="first number" type="number"><input class="input_2" placeholder="second number" type="number"></input><button>calculate</button>';
-document.body.append($task2);
-let $btn2 = $task2.querySelector("button");
-let $result2 = document.createElement("div");
-$result2.innerHTML = "<h1></h1>";
-$result2.style = "font-size: 40px";
-document.body.append($result2);
+// let $task2 = document.createElement("div");
+// $task2.classList.add("task2");
+// $task2.innerHTML =
+//   '<input class="input_1" placeholder="first number" type="number"><input class="input_2" placeholder="second number" type="number"></input><button>calculate</button>';
+// document.body.append($task2);
+// let $btn2 = $task2.querySelector("button");
+// let $result2 = document.createElement("div");
+// $result2.innerHTML = "<h1></h1>";
+// $result2.style = "font-size: 40px";
+// document.body.append($result2);
 
-let getResult2 = () => {
-  let num1 = Number ($task2.querySelector(".input_1").value)
-  let num2 = Number ($task2.querySelector(".input_2").value)
-  
-  while (num2 !== 0)
-  num2 = num1 % (num1 = num2);
+// let getResult2 = () => {
+//   let num1 = Number($task2.querySelector(".input_1").value);
+//   let num2 = Number($task2.querySelector(".input_2").value);
+
+//   while (num2 !== 0) num2 = num1 % (num1 = num2);
+
+//   $result2.innerHTML = `greatest common factor ${num1}`;
+// };
+
+// $btn2.onclick = getResult2;
+
+// 3.3;
+
+// let $title3 = document.createElement("h1");
+// $title3.innerHTML =
+//   "Запросить у пользователя число и вывести все делители этого числа.";
+// document.body.append($title3);
+
+// let $task3 = document.createElement("div");
+// $task3.classList.add("task3");
+// $task3.innerHTML =
+//   '<input class="input_3" placeholder="your number" type="number"></input><button>calculate</button>';
+// document.body.append($task3);
+// let $btn3 = $task3.querySelector("button");
+// let $result3 = document.createElement("div");
+// $result3.innerHTML = "<h1></h1>";
+// $result3.style = "font-size: 40px";
+// document.body.append($result3);
+
+// let getResult3 = () => {
+//   let num = Number($task3.querySelector(".input_3").value);
+//   let divisors = "is";
+
+//   for (let i = 1; i <= Math.round(num / 2); i++)
+//     if (!(num % i)) divisors += i + ", ";
+
+//   $result3.innerHTML = `divisors of this number ${divisors}`;
+// };
+
+// $btn3.onclick = getResult3;
+
+// 3.4;
+
+// let $title4 = document.createElement("h1");
+// $title4.innerHTML = "Определить количество цифр в введенном числе.";
+// document.body.append($title4);
+
+// let $task4 = document.createElement("div");
+// $task4.classList.add("task4");
+// $task4.innerHTML =
+//   '<input class="input_4" placeholder="your number" type="number"></input><button>calculate</button>';
+// document.body.append($task4);
+// let $btn4 = $task4.querySelector("button");
+// let $result4 = document.createElement("div");
+// $result4.innerHTML = "<h1></h1>";
+// $result4.style = "font-size: 40px";
+// document.body.append($result4);
+
+// let getResult4 = () => {
+//   let num = $task4.querySelector(".input_4").value;
+//   $result4.innerHTML = `The amount of digits is ${num.length}`;
+// };
+
+// $btn4.onclick = getResult4;
+
+// 3.5;
+
+// let $title5 = document.createElement("h1");
+// $title5.innerHTML =
+//   "Запросить у пользователя 10 чисел и подсчитать, сколько он ввел положительных, отрицательных и нулей. При этом также посчитать, сколько четных и нечетных. Вывести статистику на экран. Учтите, что достаточно одной переменной (не 10) для ввода чисел пользователем.";
+// document.body.append($title5);
+
+// let $task5 = document.createElement("div");
+// $task5.classList.add("task5");
+// $task5.innerHTML =
+//   '<input class="input_5" placeholder="your number" type="text"></input><button>calculate</button>';
+// document.body.append($task5);
+// let $btn5 = $task5.querySelector("button");
+// $btn5.onclick = addNum;
+// let $result5 = document.createElement("div");
+// $result5.innerHTML = "<h1></h1>";
+// $result5.style = "font-size: 40px";
+// document.body.append($result5);
+// let num = [];
+
+// function addNum() {
+//   num.push(Number($task5.querySelector(".input_5").value));
+//   console.log(num);
+
+//   if (num.length == 10) {
+//     let numNull = 0,
+//       numNeg = 0,
+//       numPos = 0,
+//       numEven = 0,
+//       numOdd = 0;
+//     for (let i = 0; i < 10; i++) {
+//       let $num = num[i];
+//       if ($num > 0) numPos++;
+//       else if ($num == 0 || $num == null) numNull++;
+//       else numNeg++;
+//       if ($num % 2) numOdd++;
+//       else numEven++;
+//     }
+//     $result5.innerHTML = `The amount of Positive numbers is ${numPos}. \nThe amount of Zeros is ${numNull}. \nThe amount of Negative numbers is ${numNeg} \n\nThe amount of Even numbers is ${numEven}. \nThe amount of Odd numbers is ${numOdd}.`;
+//   }
+// }
+
+// 3.6;
+
+// let $title6 = document.createElement("h1");
+// $title6.innerHTML =
+//   "Зациклить калькулятор. Запросить у пользователя 2 числа и знак, решить пример, вывести результат и спросить, хочет ли он решить еще один пример. И так до тех пор, пока пользователь не откажется.";
+// document.body.append($title6);
+
+// let $task6 = document.createElement("div");
+// $task6.classList.add("task6");
+// $task6.innerHTML =
+//   '<input class="input_61" placeholder="first number" type="number"><input class="input_62" placeholder="second number" type="number"></input><input class="input_63" placeholder="math operation" type="text"></input></input><button>calculate</button>';
+// document.body.append($task6);
+// let $btn6 = $task6.querySelector("button");
+// let $result6 = document.createElement("div");
+// $result6.innerHTML = "<h1></h1>";
+// $result6.style = "font-size: 40px";
+// document.body.append($result6);
+
+// let getResult6 = () => {
+//   let result = 0
+//   let num1 = Number($task6.querySelector(".input_61").value);
+//   let num2 = Number($task6.querySelector(".input_62").value);
+//   let operator = $task6.querySelector(".input_63").value;
+
+//   if (operator === "+") resNum = num1 + num2;
+//   else if (operator === "-") result = num1 - num2;
+//   else if (operator === "*") result = num1 * num2;
+//   else if (operator === "/") result = (num1 / num2).toFixed(2);
+//   $result6.innerHTML = `Your result is ${result} <button>one more?</button>`;
+// };
+
+// $btn6.onclick = getResult6;
+
+// 3.7
+
+// let $title7 = document.createElement("h1");
+// $title7.innerHTML =
+//   "Запросить у пользователя число и на сколько цифр его сдвинуть. Сдвинуть цифры числа и вывести результат (если число 123456 сдвинуть на 2 цифры, то получится 345612).";
+// document.body.append($title7);
+
+// let $task7 = document.createElement("div");
+// $task7.classList.add("task7");
+// $task7.innerHTML =
+//   '<input class="input_71" placeholder="number" type="number"><input class="input_72" placeholder="shift" type="number"></input></input><button>calculate</button>';
+// document.body.append($task7);
+// let $btn7 = $task7.querySelector("button");
+// let $result7 = document.createElement("div");
+// $result7.innerHTML = "<h1></h1>";
+// $result7.style = "font-size: 40px";
+// document.body.append($result7);
+
+// let getResult7 = () => {
+//   let num1 = $task7.querySelector(".input_71").value
+//   let num2 = $task7.querySelector(".input_72").value
+
+//   let i = num1.length > 0 ? num2 % num1.length : 0
+ 
+//   $result7.innerHTML = num1.slice(i)+num1.slice(0, i)
+// }
+
+// $btn7.onclick = getResult7;
+
+// 3.8
+
+// let $title8 = document.createElement("h1");
+// $title8.innerHTML =
+//   "Зациклить вывод дней недели таким образом: «День недели. Хотите увидеть следующий день?» и так до тех пор, пока пользователь нажимает OK.";
+// document.body.append($title8);
+
+// let $task8 = document.createElement("div");
+// $task8.classList.add("task8");
+// $task8.innerHTML =
+//   '<button>Next day</button>';
+// document.body.append($task8);
+// let $btn8 = $task8.querySelector("button");
+// let $result8 = document.createElement("div");
+// $result8.innerHTML = "<h1></h1>";
+// $result8.style = "font-size: 40px";
+// document.body.append($result8);
+// let currDay = 0;
+
+// let getResult8 = () => {
+//   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday?'];
+
+//   $result8.innerHTML = `today "${days[currDay]}" Wanna see the next day? click on the button to continue`
+// currDay = (currDay + 1) % days.length
+ 
+// }
+
+// $btn8.onclick = getResult8;
+
+// 3.9
+
+// let $title9 = document.createElement("h1");
+// $title9.innerHTML =
+//   "Вывести таблицу умножения для всех чисел от 2 до 9. Каждое число необходимо умножить на числа от 1 до 10.";
+// document.body.append($title9);
+
+// let $task9 = document.createElement("div");
+// $task9.classList.add("task9");
+// $task9.innerHTML =
+//   '<button>click to see multiplication table</button>';
+// document.body.append($task9);
+// let $btn9 = $task9.querySelector("button");
+// let $result9 = document.createElement("div");
+// $result9.innerHTML = "<h1></h1>";
+// $result9.style = "font-size: 40px;";
+// document.body.append($result9);
+
+
+// let getResult9 = () => {
+
+//   for (let i = 2; i < 10; i++) {
     
-  $result2.innerHTML = `greatest common factor ${num1}`;
-};
+//     for (let x = 1; x < 10; x++)
+//         $result9.innerHTML += `${i} * ${x} = ${i * x}` + '<br />'
+// }
+// }
 
-$btn2.onclick = getResult2;
+// $btn9.onclick = getResult9;
 
-3.3
+// 3.10
 
-let $title3 = document.createElement("h1");
-$title3.innerHTML =
-  "Запросить у пользователя число и вывести все делители этого числа.";
-document.body.append($title3);
+// let $title10 = document.createElement("h1");
+// $title10.innerHTML =
+//   "Вывести таблицу умножения для всех чисел от 2 до 9. Каждое число необходимо умножить на числа от 1 до 10.";
+// document.body.append($title10);
 
-let $task3 = document.createElement("div");
-$task3.classList.add("task3");
-$task3.innerHTML =
-  '<input class="input_3" placeholder="your number" type="number"></input><button>calculate</button>';
-document.body.append($task3);
-let $btn3 = $task3.querySelector("button");
-let $result3 = document.createElement("div");
-$result3.innerHTML = "<h1></h1>";
-$result3.style = "font-size: 40px";
-document.body.append($result3);
+// let $task10 = document.createElement("div");
+// $task10.classList.add("task10");
+// $task10.innerHTML =
+//   '<button> i want to play the game</button>';
+// document.body.append($task10);
+// let $btn10 = $task10.querySelector("button");
+// let $result10 = document.createElement("div");
+// $result10.innerHTML = "<h1></h1>";
+// $result10.style = "font-size: 40px;";
+// document.body.append($result10);
 
-let getResult3 = () => {
- let num = Number ($task3.querySelector(".input_3").value)
-let divisors = 'hello'
 
- for (let i = 1; i <= Math.round(num / 2); i++)
-        if (!(num % i)) divisors += i + ', '
+// let getResult10 = () => {
+// let a = 0
+// let b = 100
+// let x, y
+// while (b - a > 0) {
+//     x = Math.ceil((a + b) / 2)
+//     y = prompt(`Your number >${x}, <${x} or =${x}`)
+//     if (y == `>`) a = x
+//     if (y == `<`) b = x
+//     if (y == `=`) break
+// }
+// return $result10.innerHTML = (`The number is ${x}`)
+//     }
+  
 
-        $result3.innerHTML = `divisors of this number ${divisors}`;
-
-}
-
-$btn3.onclick = getResult3;
-
+//     $btn10.onclick = getResult10; 
---
+++ script4.js
@@ -0,0 +1,342 @@
+"use strict";
+
+4.1;
+
+let $title4_1 = document.createElement("h1");
+$title4_1.innerHTML =
+  "Написать функцию, которая принимает 2 числа и возвращает -1, если первое меньше, чем второе; 1 – если первое больше, чем второе; и 0 – если числа равны.";
+document.body.append($title4_1);
+
+let $task4_1 = document.createElement("div");
+$task4_1.classList.add("task4_1");
+$task4_1.innerHTML =
+  '<input class="input4_1" placeholder="first number" type="number"><input class="input4_12" placeholder="second number" type="number"></input><button>calculate</button>';
+document.body.append($task4_1);
+let $btn4_1 = $task4_1.querySelector("button");
+let $result4_1 = document.createElement("div");
+$result4_1 = document.createElement("h1");
+$result4_1.style = "font-size: 40px";
+document.body.append($result4_1);
+
+let getResult4_1 = () => {
+  let num1 = Number($task4_1.querySelector(".input4_1").value);
+  let num2 = Number($task4_1.querySelector(".input4_12").value);
+
+  if (num1 < num2) {
+    $result4_1.innerHTML = `-1`;
+  } else if (num1 > num2) {
+    $result4_1.innerHTML = `1`;
+  } else $result4_1.innerHTML = `0`;
+};
+
+$btn4_1.onclick = getResult4_1;
+
+4.2;
+
+let $title4_2 = document.createElement("h1");
+$title4_2.innerHTML =
+  "Написать функцию, которая вычисляет факториал переданного ей числа.";
+document.body.append($title4_2);
+
+let $task4_2 = document.createElement("div");
+$task4_2.classList.add("task4_2");
+$task4_2.innerHTML =
+  '<input class="input4_2" placeholder="Enter number" type="number"></input><button>calculate</button>';
+document.body.append($task4_2);
+let $btn4_2 = $task4_2.querySelector("button");
+let $result4_2 = document.createElement("div");
+$result4_2 = document.createElement("h1");
+$result4_2.style = "font-size: 40px";
+document.body.append($result4_2);
+
+let fact = (num1) => {
+  return num1 != 1 ? num1 * fact(num1 - 1) : 1;
+};
+let getResult4_2 = () => {
+  let num = Number($task4_2.querySelector(".input4_2").value);
+  $result4_2.innerHTML = fact(num);
+};
+
+$btn4_2.onclick = getResult4_2;
+
+4.3;
+
+let $title4_3 = document.createElement("h1");
+$title4_3.innerHTML =
+  "Написать функцию, которая принимает три отдельные цифры и превращает их в одно число. Например: цифры 1, 4, 9 превратятся в число 149.";
+document.body.append($title4_3);
+
+let $task4_3 = document.createElement("div");
+$task4_3.classList.add("task4_3");
+$task4_3.innerHTML =
+  '<input class="input4_41" placeholder="Enter number" type="number"><input class="input4_42" placeholder="Enter number" type="number"><input class="input4_43" placeholder="Enter number" type="number"></input><button>calculate</button>';
+document.body.append($task4_3);
+let $btn4_3 = $task4_3.querySelector("button");
+let $result4_3 = document.createElement("div");
+$result4_3 = document.createElement("h1");
+$result4_3.style = "font-size: 40px";
+document.body.append($result4_3);
+
+let getResult4_3 = () => {
+  let num1 = $task4_3.querySelector(".input4_41").value;
+  let num2 = $task4_3.querySelector(".input4_42").value;
+  let num3 = $task4_3.querySelector(".input4_43").value;
+
+  $result4_3.innerHTML = String(num1) + String(num2) + String(num3);
+};
+
+$btn4_3.onclick = getResult4_3;
+
+4.4;
+
+let $title4_4 = document.createElement("h1");
+$title4_4.innerHTML =
+  "Написать функцию, которая принимает длину и ширину прямоугольника и вычисляет его площадь. Если в функцию передали 1 параметр, то она вычисляет площадь квадрата.";
+document.body.append($title4_4);
+
+let $task4_4 = document.createElement("div");
+$task4_4.classList.add("task4_4");
+$task4_4.innerHTML =
+  '<input class="input4_41" placeholder="first number" type="number"><input class="input4_42" placeholder="second number" type="number"></input><button>calculate</button>';
+document.body.append($task4_4);
+let $btn4_4 = $task4_4.querySelector("button");
+let $result4_4 = document.createElement("div");
+$result4_4 = document.createElement("h1");
+$result4_4.style = "font-size: 40px";
+document.body.append($result4_4);
+
+let getResult4_4 = () => {
+  let num1 = Number($task4_4.querySelector(".input4_41").value);
+  let num2 = Number($task4_4.querySelector(".input4_42").value);
+
+  if (num1 == "") num1 = num2;
+  else if (num2 == "") num2 = num1;
+  $result4_4.innerHTML = `The area of the rectangle is ${
+    num1 * num2
+  }. Have a good day, keep calm and learn JS`;
+};
+
+$btn4_4.onclick = getResult4_4;
+
+4.5;
+
+let $title4_5 = document.createElement("h1");
+$title4_5.innerHTML =
+  "Написать функцию, которая проверяет, является ли переданное ей число совершенным. Совершенное число – это число, равное сумме всех своих собственных делителей.";
+document.body.append($title4_5);
+
+let $task4_5 = document.createElement("div");
+$task4_5.classList.add("task4_5");
+$task4_5.innerHTML =
+  '<input class="input4_5" placeholder="Enter number" type="number"></input><button>calculate</button>';
+document.body.append($task4_5);
+let $btn4_5 = $task4_5.querySelector("button");
+let $result4_5 = document.createElement("div");
+$result4_5 = document.createElement("h1");
+$result4_5.style = "font-size: 40px";
+document.body.append($result4_5);
+
+let getResult4_5 = () => {
+  let num = Number($task4_5.querySelector(".input4_5").value);
+
+  let divisors = 0;
+  for (let i = 1; i <= Math.round(num / 2); i++) if (!(num % i)) divisors += i;
+  if (num == divisors) {
+    $result4_5.innerHTML = `This number is perfect`;
+  } else {
+    $result4_5.innerHTML = `This number is not perfect`;
+  }
+};
+
+$btn4_5.onclick = getResult4_5;
+
+4.6;
+
+let $title4_6 = document.createElement("h1");
+$title4_6.innerHTML =
+  "Написать функцию, которая принимает минимальное и максимальное значения для диапазона, и выводит только те числа из диапазона, которые являются совершенными. Используйте написанную ранее функцию, чтобы узнавать, совершенное число или нет. ";
+document.body.append($title4_6);
+
+let $task4_6 = document.createElement("div");
+$task4_6.classList.add("task4_6");
+$task4_6.innerHTML =
+  '<input class="input4_61" placeholder="first number" type="number"><input class="input4_62" placeholder="second number" type="number"></input><button>calculate</button>';
+document.body.append($task4_6);
+let $btn4_6 = $task4_6.querySelector("button");
+let $result4_6 = document.createElement("div");
+$result4_6 = document.createElement("h1");
+$result4_6.style = "font-size: 40px";
+document.body.append($result4_6);
+
+function perfectNumber(num) {
+  let x = 0;
+  for (let i = 1; i <= Math.round(num / 2); i++) if (!(num % i)) x += i;
+  if (num == x) return "+";
+  return "-";
+}
+
+let getResult4_6 = () => {
+  let num1 = Number($task4_6.querySelector(".input4_61").value);
+  let num2 = Number($task4_6.querySelector(".input4_62").value);
+
+  let num3 = " ";
+  for (let i = num1; i <= num2; i++)
+    if (perfectNumber(i) == "+") num3 += i + ", ";
+  if (num3 === " ")
+    $result4_6.innerHTML = `There are no perfect numbers in this range`;
+  else $result4_6.innerHTML = `Perfect numbers ${num3}`;
+};
+
+$btn4_6.onclick = getResult4_6;
+
+4.7;
+
+let $title4_7 = document.createElement("h1");
+$title4_7.innerHTML =
+  "Написать функцию, которая принимает время (часы, минуты, секунды) и выводит его на экран в формате «чч:мм:сс».Если при вызове функции минуты и/или секунды не были переданы, то выводить их как 00.";
+document.body.append($title4_7);
+
+let $task4_7 = document.createElement("div");
+$task4_7.classList.add("task4_7");
+$task4_7.innerHTML =
+  '<input class="input4_71" placeholder="hours" type="number"><input class="input4_72" placeholder="minutes" type="number"><input class="input4_73" placeholder="seconds" type="number"></input><button>calculate</button>';
+document.body.append($task4_7);
+let $btn4_7 = $task4_7.querySelector("button");
+let $result4_7 = document.createElement("div");
+$result4_7 = document.createElement("h1");
+$result4_7.style = "font-size: 40px";
+document.body.append($result4_7);
+
+// let setTime = h => {
+//   return (m = 0) => {
+//     return (s = 0) => {
+//       return `${h}:${m}:${s}`
+//     }
+//   }
+// }
+
+let getResult4_7 = () => {
+  let h = Number($task4_7.querySelector(".input4_71").value)
+  let m = Number($task4_7.querySelector(".input4_72").value)
+  let s = Number($task4_7.querySelector(".input4_73").value)
+
+  if (h < 10) h = "0" + h
+    if (m < 10) m = "0" + m
+    if (s < 10) s = "0" + s
+    $result4_7.innerHTML = `Time ${h}:${m}:${s}`
+  // let x = setTime(h1)(m1)(s1);
+  // $result4_7.innerHTML = `Time ${x}`;
+};
+
+$btn4_7.onclick = getResult4_7;
+
+4.8
+
+let $title4_8 = document.createElement("h1");
+$title4_8.innerHTML =
+  "Написать функцию, которая принимает часы, минуты и секунды и возвращает это время в секундах.";
+document.body.append($title4_8);
+
+let $task4_8 = document.createElement("div");
+$task4_8.classList.add("task4_8");
+$task4_8.innerHTML =
+  '<input class="input4_81" placeholder="hours" type="number"><input class="input4_82" placeholder="minutes" type="number"><input class="input4_83" placeholder="seconds" type="number"></input><button>calculate</button>';
+document.body.append($task4_8);
+let $btn4_8 = $task4_8.querySelector("button");
+let $result4_8 = document.createElement("div");
+$result4_8 = document.createElement("h1");
+$result4_8.style = "font-size: 40px";
+document.body.append($result4_8);
+
+let getResult4_8 = () => {
+  let h = Number($task4_8.querySelector(".input4_81").value)
+  let m = Number($task4_8.querySelector(".input4_82").value)
+  let s = Number($task4_8.querySelector(".input4_83").value)
+  $result4_8.innerHTML = `time in seconds ${h * 3600 + m * 60 + s * 1}`
+};
+
+$btn4_8.onclick = getResult4_8;
+
+4.9
+
+let $title4_9 = document.createElement("h1");
+$title4_9.innerHTML =
+  "Написать функцию, которая проверяет, является ли переданное ей число совершенным. Совершенное число – это число, равное сумме всех своих собственных делителей.";
+document.body.append($title4_9);
+
+let $task4_9 = document.createElement("div");
+$task4_9.classList.add("task4_9");
+$task4_9.innerHTML =
+  '<input class="input4_9" placeholder="30 seconds to Mars" type="number"></input><button>calculate</button>';
+document.body.append($task4_9);
+let $btn4_9 = $task4_9.querySelector("button");
+let $result4_9 = document.createElement("div");
+$result4_9 = document.createElement("h1");
+$result4_9.style = "font-size: 40px";
+document.body.append($result4_9);
+
+let getResult4_9 = () => {
+  let s= Number($task4_9.querySelector(".input4_9").value);
+  let h = (s - s % 3600) / 3600,
+        m = s - h * 3600
+    m = (m - m % 60) / 60
+    s = s - h * 3600 - m * 60
+
+    
+  if (h < 10) h = "0" + h
+  if (m < 10) m = "0" + m
+  if (s < 10) s = "0" + s
+
+    $result4_9.innerHTML = `Time ${h}:${m}:${s}` 
+}
+
+$btn4_9.onclick = getResult4_9
+
+4.10
+
+
+let $title4_10 = document.createElement("h1");
+$title4_10.innerHTML =
+  "Написать функцию, которая принимает часы, минуты и секунды и возвращает это время в секундах.";
+document.body.append($title4_10);
+
+let $task4_10 = document.createElement("div");
+$task4_10.classList.add("task4_10");
+$task4_10.innerHTML =
+  '<input class="input4_101" placeholder="hours" type="number"><input class="input4_102" placeholder="minutes" type="number"><input class="input4_103" placeholder="seconds" type="number"></input><button>calculate</button>';
+document.body.append($task4_10);
+
+let $task4_11 = document.createElement("div");
+$task4_11.classList.add("task4_10");
+$task4_11.innerHTML =
+  '<input class="input4_111" placeholder="hours" type="number"><input class="input4_112" placeholder="minutes" type="number"><input class="input4_113" placeholder="seconds" type="number"></input>';
+document.body.append($task4_11);
+let $btn4_10 = $task4_10.querySelector("button");
+let $result4_10 = document.createElement("div");
+$result4_10 = document.createElement("h1");
+$result4_10.style = "font-size: 40px";
+document.body.append($result4_10);
+
+let getResult4_10 = () => {
+  let h = Number($task4_10.querySelector(".input4_101").value)
+  let m = Number($task4_10.querySelector(".input4_102").value)
+  let s = Number($task4_10.querySelector(".input4_103").value)
+  let h1 = Number($task4_11.querySelector(".input4_111").value)
+  let m1 = Number($task4_11.querySelector(".input4_112").value)
+  let s1 = Number($task4_11.querySelector(".input4_113").value)
+
+  let s2 = h1 * 3600 + m1 * 60 + s1 * 1 - h * 3600 + m * 60 + s * 1
+let h2 = (s2 - s2 % 3600) / 3600,
+        m2 = s2 - h2 * 3600
+    m2 = (m2 - m2 % 60) / 60
+    s2 = s2 - h2 * 3600 - m * 60
+
+    
+  if (h2 < 10) h = "0" + h
+  if (m2 < 10) m = "0" + m
+  if (s2 < 10) s = "0" + s
+
+    $result4_10.innerHTML = `Difference ${h}:${m}:${s}` 
+};
+
+$btn4_10.onclick = getResult4_10;
